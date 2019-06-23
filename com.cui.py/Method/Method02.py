cc749097bf121d6e09cd33b781bcee3b882fa76d
cui7x <cui7dr@gmail.com>
2019-06-23 22:01:48 +0800

    测试函数02——同一文件调用函数

--- com.cui.py/Method/Method02.py
+++ com.cui.py/Method/Method02.py
@@ -1,8 +1,17 @@
 # 测试函数02
+
+name = "小明"
+
+
+# 定义一个函数
 def Method02():
     print("hello 01")
     print("hello 02")
     print("hello 03")
 
 
+print(name)
+
+# 只有在程序中主动调用函数才会执行
 Method02()
+print(name)
