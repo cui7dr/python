c660b87e67adc24d2895cfb56d9ba94875339256
cui7x <cui7dr@gmail.com>
2019-07-15 22:53:38 +0800

    变量定义及简单运算

--- com.cui.py/Hello/String.py
+++ com.cui.py/Hello/String.py
@@ -8,5 +8,18 @@ str2 = '我的外号是"大黄瓜"'
 print(str1)
 print(str2)
 
+# 字符串的遍历
 for char in str2:
     print(char)
+
+# 字符串长度
+print(len(str1))
+
+# 某个字符串出现的次数
+print(str1.count("o"))
+print(str1.count("123"))
+
+# 某个字符串出现的位置
+print(str1.index("py"))
+# 如果使用 index 传递的字符串不存在会报错
+#print(str1.index("aaa"))
