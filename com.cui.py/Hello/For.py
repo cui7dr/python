da691cbd6f100524a2d77c8eaff0ac510f359edd
cui7x <cui7dr@gmail.com>
2019-07-18 21:45:07 +0800

    变量定义及简单运算

--- com.cui.py/Hello/For.py
+++ com.cui.py/Hello/For.py
@@ -10,3 +10,18 @@ for num in [1, 2, 3]:
     else:
         print("不会执行")
 print("over")
+
+# 遍历字典列表
+students = [{"name": "张三"}, {"name": "李四"}]
+print(students)
+
+# 在学生列表搜索指定的名字
+find_name = "王五"
+for stu_dict in students:
+    print(stu_dict)
+    if stu_dict["name"] == find_name:
+        print("找到了 %s" % find_name)
+        break
+    else:
+        print("没有找到 %s" % find_name)
+print("over")
