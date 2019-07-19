c49514c94b45d23684bc0b75abdf7940b213f00a
cui7x <cui7dr@gmail.com>
2019-07-19 22:01:03 +0800

    名片管理相关操作

--- com.cui.py/CardManagement/Input&Pass.py
+++ com.cui.py/CardManagement/Input&Pass.py
@@ -1,11 +1,24 @@
-# 无限循环，由用户决定退出
+"""
+  名片管理
+"""
+
+# 02 ---- 添加无限循环，由用户决定退出
 while True:
+    # 01 ---- start
     action_str = input("请输入你希望执行的操作：")
     print("你选择的操作是【%s】" % action_str)
 
     # 1，2，3 操作选项
     if action_str in ["1", "2", "3"]:
-        pass
+        # pass
+        # 03 ---- start 添加嵌套操作
+        if action_str == "1":
+            pass
+        elif action_str == "2":
+            pass
+        elif action_str == "3":
+            pass
+        # 03 ---- end
     # 0 退出
     elif action_str == "0":
         print("欢迎再次使用")
@@ -16,3 +29,4 @@ while True:
     # 程序运行时，pass 关键字不会执行任何的操作！
     else:
         print("你输入的不正确，请重新输入")
+    # 01 ---- end
